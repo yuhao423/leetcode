93fa0bde68f133a24b13d7ac694231997438c771
yuhao <1772156189@qq.com>
2024-05-13 18:00:57 +0800

    fix: l-06

--- l-06Z字形变换/index.js
+++ l-06Z字形变换/index.js
@@ -20,20 +20,23 @@ const convert = function(s, numRows) {
     let random = 2 * numRows - 2
     let rows = new Array(numRows).fill('')  //申请一个长度为 numRows 的快数组，里面每一项按索引成为每一行的数据
         /**  2         3            4
-         * a   c    a     e     a
-         * b   d    b  d    
-         *          c
-         */         
+         * a   c    a     e     a       g
+         * b   d    b  d        b    f  h
+         *          c           c  e 
+         *                      d   
+         */                     
 
         for(let i = 0;i<s.length;i++){
 
             //random = 4
-            const x = i % random
-            let min = Math.min(x,random-x)  //确保每一项应该添加到的相应行
+            const x = i % random    //numRows 是4，那么x为（0，1，2，3，4，5）,random 6
+            console.log(x);
+            let min = Math.min(x,random-x)  //确保每一项应该添加到的相应行,比如 numRows 是 4，x就会是（0，1，2，3，4，5）,random 6,保证 min 不超过3，即rows的索引
             console.log(min);
             rows[Math.min(x,random - x)] += s[i]
             console.log(rows);
         }
+        return rows.join('')
     }
 };
-convert('sudguwef',3)
+console.log(convert('sudguwefwefwe',4));
