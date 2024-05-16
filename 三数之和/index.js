029ad0d670b9e39951a8c584dc02e3e8e974314f
yuhao <1772156189@qq.com>
2024-05-16 20:13:41 +0800

    feat: add l-15三数之和

---
+++ 三数之和/index.js
@@ -0,0 +1,36 @@
+
+
+
+
+/**
+ * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+
+    你返回所有和为 0 且不重复的三元组。
+
+    注意：答案中不可以包含重复的三元组。
+
+ 
+ */
+
+    /**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+
+    let map = new Map()
+    let res = []
+    for(let i = 0;l<nums.length;i++){  //不用循环到结束啊
+
+    let len1 = nums[i]
+
+
+        if(map.get(nums[i])){
+
+        }
+
+
+        map.set(nums[i],i)
+    }
+
+};
