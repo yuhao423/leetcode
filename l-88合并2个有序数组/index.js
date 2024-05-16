7e089de8bfcc86b854380e3e343701ffeed602d1
yuhao <1772156189@qq.com>
2024-05-16 17:58:19 +0800

    fix: 修复l-88

--- l-88合并2个有序数组/index.js
+++ l-88合并2个有序数组/index.js
@@ -33,9 +33,49 @@ console.log('ssseeff'.split('e').join(''));
 var remerge = function(nums1, m, nums2, n){
 
 
-    //按
-    let len1 = 0 ;
-    let 
+    
+    let len1 = 0;
+    let len2 = 0;
+    let len = 0;
+
+    while(len2 <= n - 1){
 
 
+        if(len1 > m - 1){  //len1 > 2
+            //就不需要继续走下面的了
+            console.log(len,len2);
+            nums1[len++] = nums2[len2++]
+            continue;
+        }
+        nums1[len++] = nums1[len1] <= nums2[len2] ? nums1[len1++] : nums2[len2++];
+        // console.log(len,len1,len2,  nums1,'3');
+    }
+    console.log(len,len1,len2,  nums1,'3');
 }
+// console.log(remerge([1,2,3,0,0,0],3,[2,5,6],3));  //有一点点的小问题
+//1
+//1 2 
+//1 2 2 
+
+
+const test = (nums1, m, nums2, n)=>{
+
+    //使用双指针来解决这个问题
+    let len1 = m - 1;  //nums1中 m 的索引
+    let len2 = n - 1;  //nums2中 最后一个的索引
+    let len = m + n - 1; // nums1中数组中最后一个的索引
+
+    while(len2 >= 0){  //写whlie循环一定要考虑的是 什么条件能打破循环
+
+        if(len1 < 0){
+            //这时候无需再进行下面的比较了，因为 nums2 是有序的，直接插进入数组就行了
+            nums1[len--] = nums2[len2--]
+            continue;  //不是打破循环，打破循环用的是 break ，这里的 continue 是终止下面的语句，直接进行下一轮循环
+        }
+
+        nums1[len--] = nums1[len1] >= nums2[len2] ? nums1[len1--] : nums2[len2--]  //依次确定最大的
+
+    }
+    console.log(nums1);
+}   
+console.log(test([1,2,3,0,0,0],3,[2,5,6],3));
